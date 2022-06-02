07f98976bcb91d12041787121266144c8ee59720
Wdyla Epaminondas Carvalho <wdyla.carvalho@avanade.com>
2022-06-02 11:42:44 -0300

    tela inicial

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
+import { NativeBaseProvider, Box, Button, Text, ScrollView, Stack, Input, FormControl, Heading } from 'native-base';
 
 export default function App() {
+
+  const Botao = () => {
+    return <Box alignItems="center">
+        <Button
+         style={styles.botao}
+         onPress={() => console.log("hello world")}>Denuncie</Button>
+      </Box>;
+  };
+
   return (
+    <NativeBaseProvider>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Image 
+    style={styles.image}
+    source={require('./assets/logo.png')}
+    />
+    <Box style={styles.inicio}>
+    <Heading color="white">Cyberbullyling</Heading>
+    <Text style={styles.text}>Cyberbullying é o bullying realizado por meio das tecnologias digitais. 
+      Pode ocorrer nas mídias sociais, plataformas de mensagens, plataformas de jogos 
+      e celulares. É o comportamento repetido, com intuito de assustar, enfurecer ou 
+      envergonhar aqueles que são vítimas.</Text>
+      <Image 
+      style={styles.imagebox}
+      source={require('./assets/foto.jpg')}
+      />
+      </Box>
+      <Botao></Botao>
       <StatusBar style="auto" />
     </View>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#48378e'
   },
+  image: {
+    width: 190,
+    height: 70,
+  },
+  botao:{
+    backgroundColor: "#FF0000",
+    width: 70,
+    height: 32,
+  },
+  text: {
+    color: 'white',
+    width: 300,
+    height: 20,
+    margin: 10,
+  },
+  inicio: {
+    alignItems: 'center',
+    margin: 10,
+  },
+  imagebox: {
+    width: 300,
+    height: 200,
+    margin: 50,
+  }
 });
