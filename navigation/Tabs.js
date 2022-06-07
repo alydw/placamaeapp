105b5bceaebd9064c9b6bb003c6aadd831df1e71
Wdyla Epaminondas Carvalho <wdyla.carvalho@avanade.com>
2022-06-07 14:03:12 -0300

    ndjs

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,11 +3,12 @@ import Contato from "../pages/Contato"
 import { StyleSheet } from "react-native"
 import Home from "../pages/Home"
 
+
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
     return(
-        <Tab.Navigator initialRouteName='Contato'>
+        <Tab.Navigator initialRouteName='Home'>
             <Tab.Screen name="Home" component={Home}></Tab.Screen>
             <Tab.Screen name="Contato" component={Contato} options={{tabBarStyle : {display : 'none'}}}></Tab.Screen>
             
--- pages/Contato.js
+++ pages/Contato.js
@@ -1,13 +1,19 @@
 import { StyleSheet, View, Image, StatusBar, Text, Linking } from 'react-native';
 import { NativeBaseProvider, Box, Button,  ScrollView, Stack, Input, FormControl, Heading } from 'native-base';
-
+import { SiInstagram } from "react-icons/si";
 
 export default function Contato(){
         return(
           <View style={styles.container}>
+            <Image 
+            style={styles.image}
+            source={require('../assets/logo.png')}
+            />
+
               <Heading style={styles.head}>Contato</Heading>
-              <View style={{border : '1px solid green', height: 400}}>
+              <View style={{height: 400}}>
               <Text style={styles.text} onPress={() => Linking.openURL('https://placamae.org/')}>https://placamae.org/</Text>
+              <Text style={styles.text} onPress={() => Linking.openURL('https://www.instagram.com/placamae.org_/')}><SiInstagram color="white"/> placamae.org_</Text>
                   <Text style={styles.text}>contato@placamae.org</Text>
                   <Text style={styles.text}>Rua Marques do Herval, 167/902 Box 31,</Text>
                   <Text style={styles.text}>CEP 50020-030</Text>
@@ -22,18 +28,23 @@ export default function Contato(){
           alignItems: 'center',
           justifyContent: 'space-between',
           backgroundColor: '#48378e',
-          border : "1px solid red"
         },
-
         text : {
           color : 'white',
-          fontFamily: "Helvetica Neue"
+          fontFamily: "Helvetica Neue",
+          margin: 2,
+          padding: 2
         },
-
         head: {
-          
           color: 'white',
-          top: 0,
-        }
+          top: 60,
+        },
+        image: {
+        width: 190,
+        height: 70,
+        position: 'absolute',
+        top: 3,
+        marginBottom: 10,
+  },
         
       });
